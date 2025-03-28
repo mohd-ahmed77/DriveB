a80155c28276b473c32dc02e9b5d11ee6d672ed4
Ahmed <155244078+mohd-ahmed77@users.noreply.github.com>
2025-03-28 20:08:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,29 +2,29 @@ const express=require('express');
 const app=express();
 const port=3000;
 const path=require('path');
-
+const mongoose=require('mongoose');
+const bodyParser=require('body-parser');
+const User=require('./Models/User.js');
+const connection=require('./config/db.js');
+const userModel = require('./Models/User.js');
+ // Connect to MongoDB
+const userRouter=require('./Routes/user.routes.js');
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static("Public"));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
-app.get('/',(req,res)=>{   
-    res.render('index');
-});
+// app.get('/',(req,res)=>{   
+//     // res.render('index');
+// });
+connection(); 
+
+app.use('/user',userRouter);
 
 
 
-app.post('/formdata',(req,res)=>{   
-    console.log(req.body);
-   console.log('form data');     
-    res.send('Form data submitted successfully');
-    
-   
-    
-
-});
 
 
 
